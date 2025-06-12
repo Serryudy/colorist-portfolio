7ec883ca079278417de20837b7e2259b1e0c0f2d
Serryudy <somapalagalagedara@gmail.com>
2025-06-13 03:24:36 +0530

    g

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['your-domain.com'],
-    unoptimized: true,
-  },
-}
+    images: {
+        domains: ['images.unsplash.com'], // Add any domains you're using for images
+      },
+    webpack: (config) => {
+    config.module.rules.push({
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        use: ['raw-loader', 'glslify-loader'],
+    });
+    return config;
+    },
+    // Add if you need to handle large video files
+    experimental: {
+    largePageDataBytes: 128 * 100000, // 128KB -> 12.8MB
+    },
+};
 
-module.exports = nextConfig
+export default nextConfig;
